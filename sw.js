f923f5de007a7a14ad9951597e24ec7127ef7310
Katri Yli-Jaakkola <ylijaakkolak@gmail.com>
2026-07-02 14:43:50 +0300

    Service worker hakee aina uusimman version verkosta

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'kauppalista-v1';
+const CACHE = 'kauppalista-v2';
 const FILES = [
   '/',
   '/index.html',
@@ -11,12 +11,22 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE).then(cache => cache.addAll(FILES))
   );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(cached => {
-      return cached || fetch(event.request);
-    })
+    fetch(event.request).catch(() =>
+      caches.match(event.request)
+    )
   );
 });
